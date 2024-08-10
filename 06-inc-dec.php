0a6d5d4d87e78108040117fb1e025461413d15db
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-08-10 17:08:57 -0600

    Se agrega incremento y decremento

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,20 @@
 <?php include 'includes/header.php';
 
+$numero1 = 30;
+//El ++ le incrementa su valor en 1
+$numero1++;
+echo $numero1;
 
+echo '<br/>';
+
+// Podemos agregar 1 al mommento de imprimir poniendo ++ al inicio
+echo ++$numero1;
+echo '<br/>';
+
+$numero2 = 30;
+// Tambien podemos decrementar en 1 con --
+$numero2--;
+echo $numero2;
 
 
 include 'includes/footer.php';
