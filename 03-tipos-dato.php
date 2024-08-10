d222161b6f9a951e1845fc6418e3c1bfc991365c
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-08-10 15:27:59 -0600

    Se agregan tipos de datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,24 @@
 <?php include 'includes/header.php';
 
+//Boolean
+$logueado = true;
+var_dump($logueado);
 
+//Enteros
+$numero = 200;
+var_dump($numero);
+
+//Floats
+$float = 20.5;
+var_dump($float);
+
+//Strings
+$nombre = 'Alonso';
+var_dump($nombre);
+
+//Arreglos
+$array = [];
+var_dump($array);
 
 
 include 'includes/footer.php';
