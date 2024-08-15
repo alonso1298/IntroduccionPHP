6cf526ca987da4395d7b2d62fd777e76050052ca
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-08-14 18:49:09 -0600

    Se agrega ForEach

--- 13-loops.php
+++ 13-loops.php
@@ -38,16 +38,46 @@ echo '<br/>';
 * 3 y 5 Imprimir FizzBuzz
 */
 
-for($i = 1; $i < 1000; $i++ ) {
-    if($i % 15 === 0){
-        echo $i . ' - FizzBuzz <br/>';
-    } else if($i % 3 === 0) {
-        echo $i . ' - Fizz <br/>';
-    } else if($i % 5 === 0) {
-        echo $i . ' - Buzz <br/>';
-    } else {
-        echo $i . '<br/>';
-    }
+// for($i = 1; $i < 1000; $i++ ) {
+//     if($i % 15 === 0){
+//         echo $i . ' - FizzBuzz <br/>';
+//     } else if($i % 3 === 0) {
+//         echo $i . ' - Fizz <br/>';
+//     } else if($i % 5 === 0) {
+//         echo $i . ' - Buzz <br/>';
+//     } else {
+//         echo $i . '<br/>';
+//     }
+// }
+
+// For Each 
+$clientes = array('Pedro', 'Juan', 'Karen');
+
+foreach( $clientes as $cliente ){
+    echo $cliente . '<br/>';
+}
+
+echo '<br/>';
+
+$cliente = [
+    'nombre' => 'Alonso',
+    'saldo' => 200,
+    'tipo' => 'Premium'
+];
+
+foreach( $cliente as $key => $valor ){ //Key nos dice la clave
+    echo $key . ': '  . $valor . '<br/>';
+}
+
+echo '<br/>';
+
+//Nos da la extensión del arreglo 
+echo count($clientes);
+echo '<br/>';
+echo sizeof($clientes);
+echo '<br/>';
+for($i = 0; $i < count($clientes); $i++){
+    echo $clientes[$i] . '<br/>';
 }
 
 include 'includes/footer.php';
