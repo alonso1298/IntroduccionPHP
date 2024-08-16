49da674a965266fe10f32d82652a23cfffc6325a
alonsosagrerogranados <alonsosagrerogranados@gmail.com>
2024-08-16 15:36:14 +0000

    Se agrega types

--- 16-types.php
+++ 16-types.php
@@ -1,7 +1,11 @@
 <?php include 'includes/header.php';
 
+function usuarioAutenticado() {
+    return 'El usuario esta autenticado';
+}
 
+$usuario = usuarioAutenticado();
 
-
+echo $usuario;
 
 include 'includes/footer.php';
