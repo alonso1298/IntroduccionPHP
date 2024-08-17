96b73591f7173f0ef42c0708d2b028eaaedca7d6
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-08-17 11:35:20 -0600

    Se agrega require e include

--- 17-require.php
+++ 17-require.php
@@ -1,8 +1,14 @@
 <?php include 'includes/header.php';
 
+/*
+require: lo utilizamos cuando tengamos funciones que son criticas para la aplicacion
+inclide: se utiliza cuando se quiera incluir otros templates
+requiere_once: revisa si el archivo ya fue incluido y si ya fue incluido ignora la linea de codigo y si no la incluye
+*/
 
+require 'funciones.php';
 
-
+iniciarApp();
 
 
 include 'includes/footer.php';
