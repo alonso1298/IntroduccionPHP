70e786a1665fa342cb431e04423cbfb2977b642d
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-08-16 19:44:59 -0600

    Se agrega types

--- 16-types.php
+++ 16-types.php
@@ -1,11 +1,29 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);
+include 'includes/header.php';
 
-function usuarioAutenticado() {
-    return 'El usuario esta autenticado';
+function usuarioAutenticado(bool $autenticado) : string { // Despues de los : decimos que tipo de dato va a retornar 
+    if($autenticado) {
+        return 'El Usuario esta autenticado';
+    }else {
+        return 'No autenticado';
+    }
 }
 
-$usuario = usuarioAutenticado();
+$usuario = usuarioAutenticado(false);
 
 echo $usuario;
+echo '<br>';
+
+function esMayor($anios) : ?string{ //Al poner un sigo de ? quiere decir que puede retornar un string
+    if($anios > 18){
+        return 'Es mayor de edad';
+    }else{
+        return null;
+    }
+}
+
+$persona = esMayor(55);
+echo $persona;
 
 include 'includes/footer.php';
